3d9ae299aa6d6146079396eb6f01e3e5ce9ff9c8
r00t254 <root254.mary@gmail.com>
2022-06-12 07:16:47 +0300

    added serializer files

--- awwards/settings.py
+++ awwards/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from pyuploadcare.dj import conf as dj_conf
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
 ]
 
 MIDDLEWARE = [
