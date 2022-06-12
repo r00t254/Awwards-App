840b56e8d72f77d8c9d68ce6800c9252dc7c54d1
r00t254 <root254.mary@gmail.com>
2022-06-12 09:07:56 +0300

    add changes

--- awwards/settings.py
+++ awwards/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2mqn7ngr$_^(!k)hq2^i@v6rulj&t$zpq*eqijw_1xnpv@dcy3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
